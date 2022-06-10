c283c1d94e303e2a6b77fd46ab9742999afcb2a8
omero2582 <iiomero2582ii@gmail.com>
2022-06-10 11:17:41 -0400

    simple bootleg working version, need responsive ui
    output on console.
    broke down functions, but dont think its functional programming.
    Need to add feedback on ui on: switch turns, win, cant play on this
    spot on the board (while playing, or when game is over). Also add
    opt-in ability to change name... careful against <script>

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,12 +2,21 @@ const Gameboard = (() => {
     let board = new Array(9).fill(null);
     const getGameBoard = () => board;
     const setSquare = (index, symbol) => {
-        board[index] = symbol;
+        square = getSquare(index);
+        if (square == null){
+            console.log(`marking at ${index}`);
+            board[index] = symbol;
+            return true;   
+        }else{
+            console.log(`square at index ${index} is taken by ${square}`);
+            return false;
+        }
+        
     };
     const getSquare = (index) => board[index];
     const resetBoard = () => board = new Array(9).fill(null);
 
-    return {getGameBoard, setSquare, getSquare};
+    return {getGameBoard, setSquare, getSquare, resetBoard};
 })();
 
 
@@ -19,8 +28,9 @@ const Player = (name, symbol) =>{
     const addWins = () => ++wins;
     const setName = (newName) => name = newName;
     const setSymbol = (newSymbol) => symbol = newSymbol;
+    const resetWins = () => wins = 0;
 
-    return {getName, getSymbol, getWins, addWins, setName, setSymbol};
+    return {getName, getSymbol, getWins, addWins, setName, setSymbol, resetWins};
 };
 
 
@@ -29,39 +39,31 @@ const Game = (() => {
     let p2 = Player ('Player 2', '0');
     let playerArray = [p1, p2];
     let currentPlayer = p1;
-    let gameState;
+    let gameOver = false;
 
 
-    const markAt = (index) => {         //maybe move this into Player also ?, think of it as Turn based, and player.attack()
-                                        // eventListener should maybe start from the currentPlayer's markAt, then 
-                                        //that will ASK the game's markAt if they are allowed to play there
-                                        // NVM this would make redundant code cause the event listener would reference currentPlayer.attack()
-                                        // then this player would ask 'Game' to verify if they are currentPlayer.... which they obv are..
-                                        // new TODO: maybe put this inside Gameboard..that way we dont have to expose setSquare() from Gamebnopard
+    const markAt = (index) => {         
+        if (gameOver) {return};
+        
         let symbol = getCurrentPlayer().getSymbol();
-        let square = Gameboard.getSquare(index);
-        if (square == null){
-            console.log(`marking at ${index}`);
-            Gameboard.setSquare(index, symbol);
+        if (Gameboard.setSquare(index, symbol)){
             let winner = checkWin();
-            if (winner == "playing"){
-                //still playing
-                console.log("still playing");
-                switchPlayers();    //add css class to this player
-            }else if (winner == 'draw'){
+            if (winner){
+                gameOver = true;
+                 console.log(`${winner.getName()} wins with ${winner.getSymbol()}`);
+                 winner.addWins();
+                 displayController.renderPlayerDisplays(); 
+            }else if (checkDraw()){
+                gameOver = true;
                 console.log('draw');
             }else{
-                // someone won... how do i keep track of winners/# of wins?? on game or gameboard ? also enable a newgame button
-                console.log(`${winner.getName()} wins with ${winner.getSymbol()}`);
-                winner.addWins();
-                displayController.renderPlayerDisplays();
+                console.log("still playing");
+                switchPlayers();    //add css class to this player
             }
             displayController.renderGameBoard();
             return symbol;
-        }else{
-            console.log(`square at index ${index} is taken by ${square}`);
-            return null;
-        }
+        } //else invalid move
+
     };
 
     const switchPlayers= () =>{
@@ -73,31 +75,24 @@ const Game = (() => {
 
     const getPlayerArray = () => playerArray;
 
-    const getGameState = () => gameState;
-
-    const checkWin = () => {
-            
+    const checkWin = () => {       
             //check the 3 rows
             //0 1 2,    345,    6 7 8
             for (let i = 0; i<9; i+=3){
-                let symbol = Gameboard.getSquare(i);
-                let nextSymbols = [Gameboard.getSquare(i+1), Gameboard.getSquare(i+2)];     
-                if (symbol && nextSymbols.every( next => next == symbol)){
-                    console.log('row');
-                    //return (symbol == p1.getSymbol()) ? p1 : p2;
-                    return playerArray.find( player => symbol == player.getSymbol());
+                let indexArray = [i, i+1, i+2]; 
+                if(isSameSymbolByIndex(indexArray)){ 
+                    logSameSymbols(indexArray, 'row');
+                    return findPlayerByIndex(i);
                 }
             }
 
             //check the 3 columns
             //0 3 6,    1 4 7,  2 5 8
             for (let i = 0; i<3; i++){
-                let symbol = Gameboard.getSquare(i);
-                let nextSymbols = [Gameboard.getSquare(i+3), Gameboard.getSquare(i+6)];
-                if(symbol && nextSymbols.every( next => next == symbol)){ 
-                    console.log(`column ${i} = ${i+3} = ${i+6} with ${symbol} = ${Gameboard.getSquare(i+3)} = ${Gameboard.getSquare(i+6)}`);
-                    //return (symbol == p1.getSymbol()) ? p1: p2;
-                    return playerArray.find( player => symbol == player.getSymbol());
+                let indexArray = [i, i+3, i+6]; 
+                if(isSameSymbolByIndex(indexArray)){ 
+                    logSameSymbols(indexArray, 'column');
+                    return findPlayerByIndex(i);
                 } 
             }
 
@@ -105,26 +100,59 @@ const Game = (() => {
             //0 4 8,     2 4 6
             let j = 4;
             for (let i = 0; i<3; i+=2){
-                let symbol = Gameboard.getSquare(i);
-                let nextSymbols = [ Gameboard.getSquare(i+j), Gameboard.getSquare(i+j+j)];
-                if(symbol && nextSymbols.every( next => next == symbol) ){
-                    console.log('diagonal');
-                    //  return (symbol == p1.getSymbol()) ? p1 : p2;
-                    return playerArray.find( player => symbol == player.getSymbol());
+                let indexArray = [i, i+j, i+j+j]; 
+                if(isSameSymbolByIndex(indexArray)){ 
+                    logSameSymbols(indexArray, 'diagonal');
+                    return findPlayerByIndex(i);
                 } 
                 j -=2;    
             }
-
-            //checkDraw()
-            //if gameboard has no nulls, then end
-            if (Gameboard.getGameBoard().indexOf(null) == -1){
-                return 'draw';
-            }else{
-                return 'playing';
-            }
     };
 
-    return {markAt, switchPlayers, getCurrentPlayer, getPlayerArray};
+    const logSameSymbols = (index, string) =>{
+        console.log(`${string} ${index[0]} = ${index[1]} = ${index[2]} with ${Gameboard.getSquare(index[0])} = ${Gameboard.getSquare(index[1])} = ${Gameboard.getSquare(index[2])}`);
+    };
+
+    const isSameSymbol = (symbolArray) =>{
+        //let firstSymbol = symbolArray.shift();      //dont use even though it will comapre vs itself since shift is prob an expensive operation
+        return symbolArray[0] && symbolArray.every(symbol => symbol == symbolArray[0])
+    };
+
+    const isSameSymbolByIndex = (indexArray) => {
+        let symbolArray = [Gameboard.getSquare(indexArray[0]), Gameboard.getSquare(indexArray[1]), Gameboard.getSquare(indexArray[2])];
+        return isSameSymbol(symbolArray);
+    };
+
+    const findPlayerBySymbol = (symbol) =>{
+        return playerArray.find(player => player.getSymbol() == symbol);
+    };
+
+    const findPlayerByIndex = (index) =>{
+        return findPlayerBySymbol(Gameboard.getSquare(index));
+    }
+
+    const checkDraw = () => {
+        return Gameboard.getGameBoard().indexOf(null) == -1;
+    };
+
+    const newRound = (string) => {
+        gameOver = false;
+        Gameboard.resetBoard();
+        console.log(string);
+        displayController.renderGameBoard();
+    }
+    
+    const newGame = () =>{
+        newRound('new Game');
+        resetScore();
+        displayController.renderPlayerDisplays();
+    };
+
+    const resetScore = () =>{
+        playerArray.forEach(player => player.resetWins());
+    };
+
+    return {markAt, switchPlayers, getCurrentPlayer, getPlayerArray, newRound, newGame};
 })();
 
 const displayController = (() => {
@@ -133,6 +161,8 @@ const displayController = (() => {
     const p2Name = document.querySelector('.p2-name');
     const p1Wins = document.querySelector('.p1-wins');
     const p2Wins = document.querySelector('.p2-wins');
+    const newRound = document.querySelector('.new-round');
+    const newGame = document.querySelector('.new-game');
 
     const renderGameBoard = () => {
         grid.innerHTML = "";
@@ -155,8 +185,14 @@ const displayController = (() => {
         p2Wins.textContent = Game.getPlayerArray()[1].getWins();
     };
 
+    const renderGameFooter = () => {
+        newRound.addEventListener('click', () => Game.newRound('new Round'));
+        newGame.addEventListener('click', () => Game.newGame('new Game'));
+    };
+
     renderGameBoard();
     renderPlayerDisplays();
+    renderGameFooter();
 
     return {renderGameBoard, renderPlayerDisplays}
 })();
